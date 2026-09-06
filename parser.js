8780444a9b88f009c49c679240890155e83f61cc
G1ushkov <262115192+G1ushkov@users.noreply.github.com>
2026-09-06 17:31:35 +0500

    Добавил основные данные товара

--- parser.js
+++ parser.js
@@ -35,6 +35,73 @@ function parsePage() {
 
     // Product
 
+    const sectionProduct = document.querySelector('section.product');
+    const productId = sectionProduct.dataset.id;
+
+    const productName = sectionProduct
+        .querySelector('.title')
+        .textContent
+        .trim();
+
+    const buttonLikeStatus = sectionProduct
+        .querySelector('.like')
+        .classList.contains('active');
+
+    const price = parseInt(sectionProduct
+        .querySelector('.price')
+        .childNodes[0]
+        .textContent
+        .trim()
+        .slice(1)
+    );
+
+    const oldPrice = parseInt(sectionProduct
+        .querySelector('.price')
+        .children[0]
+        .textContent
+        .trim()
+        .slice(1)
+    );
+
+    const discount = oldPrice - price;
+
+    const discountPercent = `${(discount / oldPrice * 100).toFixed(2)}%`;
+
+    const currencySymbol = sectionProduct
+        .querySelector('.price')
+        .childNodes[0]
+        .textContent
+        .trim()
+        .slice(0, 1);
+
+    const currencyMap = {
+        '₽': 'RUB',
+        '$': 'USD',
+        '€': 'EUR'
+    };
+
+    const currency = currencyMap[currencySymbol];
+
+    const productProperties = sectionProduct.querySelectorAll('.properties li');
+
+    const properties = {};
+
+    productProperties.forEach(property => {
+        const key = property.children[0].textContent.trim();
+        const value = property.children[1].textContent.trim();
+
+        properties[key] = value;
+    });
+
+    const productDescription = sectionProduct.querySelector('.description');
+    const descriptionClone = productDescription.cloneNode(true);
+    descriptionClone.querySelectorAll('*').forEach((element) => {
+        for (const attribute of element.attributes) {
+            element.removeAttribute(attribute.name);
+        }
+    });
+    const description = descriptionClone.innerHTML.trim();
+
     // Suggested products
 
     // Reviews
